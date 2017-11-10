8f83505bdc7ad297b7762f71da7f4c1e5636d9db
ekkazit <ekkazit@gmail.com>
2017-11-10 15:55:34 +0700

    Initial commit

--- app.js
+++ app.js
@@ -11,14 +11,6 @@ app.get('/', function (req, res, next) {
     res.send('Server is working');
 });
 
-app.get('/products', function (req, res, next) {
-    res.send('Product Page');
-});
-
-app.get('/customers', function (req, res, next) {
-    res.send('Customer Page');
-});
-
 app.listen(3000, function () {
     console.log('server is listening on port ' + port);
 });
